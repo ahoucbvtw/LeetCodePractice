88e29755c5dde209880cece5d9fa969ef40baf8e
ahoucbvtw <ahoucbvtw.nw@gmail.com>
2024-04-22 23:18:54 +0800

    Update 69 Solution

--- LeetCode Questions/Easy/69_Sqrt(x)/69.py
+++ LeetCode Questions/Easy/69_Sqrt(x)/69.py
@@ -1,1 +1,17 @@
-# 
+# submitted at Apr 22, 2024 21:24
+class Solution:
+  def mySqrt(self, x: int) -> int:
+    a_point = 0
+    b_point = x
+    
+    while a_point <= b_point:
+      half = (a_point+b_point) // 2
+      
+      if half ** 2 == x:
+        return half
+      
+      if half ** 2 < x:
+        a_point = half + 1
+      else:
+        b_point = half - 1
+    return b_point
